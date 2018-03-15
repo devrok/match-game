7e496050fd9f925d12f8231d235e7752690091e3
devrok <roland.kurz@gmx.at>
2018-03-15 21:28:27 +0100

    Committing progress

--- resources/js/main/match-game.js
+++ resources/js/main/match-game.js
@@ -18,8 +18,27 @@ var MatchGame = {};
  */
 
 MatchGame.generateCardValues = function () {
+  var valuePairs = [];
 
+  for(var i = 1; i < 9 ; i++) {
+    valuePairs.push(i);
+    valuePairs.push(i);
+  }
+
+  var newArray = [];
+
+  while (valuePairs.length > 0) {
+    var randIndex = Math.floor(Math.random() * valuePairs.length);
+    console.log(randIndex);
+    newArray.push(valuePairs[randIndex]);
+    valuePairs.splice(randIndex,1);
+    console.log("Old: " + valuePairs);
+    console.log("New: " + newArray);
+  }
+
+  return newArray;
 };
+
 
 /*
   Converts card values to jQuery card objects and adds them to the supplied game
